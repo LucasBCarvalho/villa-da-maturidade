5315dde90d2aad73f492419c4af123b633e45218
DOULORES <lucas.carvalho30111@gmail.com>
2026-03-06 15:08:34 -0300

    Cria arquivo dos serviços e adiciona ele no arquivo de pages para carregar as informações

---
+++ src/app/_components/services.tsx
@@ -0,0 +1,12 @@
+
+export function Services() {
+    return (
+        <section className="bg-white py-16">
+            <div className="container mx-auto px-4">
+                <div>
+                    <h2 className="font-4x1">Serviços</h2>
+                </div>
+            </div>
+        </section>
+    )
+}
